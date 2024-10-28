9c6fe289f1dbae4ec32b644ade33607ffab11bdf
yassine <yassinejedidi2004@gmail.com>
2024-10-28 01:34:51 +0100

    Enregistrement des données météo dans le fichier meteo.txt

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+#Récupération des données
 VILLE=$1
 curl -s "wttr.in/$VILLE?format=v2" -o meteo_brute.txt
 
+#Récupération des témpératures
 TEMP_ACTUELLE=$(grep -oP '(?<=\+)\d+°C' meteo_brute.txt | head -n1)
-
 TEMP_DEMAIN=$(curl -s "wttr.in/$VILLE?format=%t&forecast=1&day=1" | grep -oP '\+\d+°C')
 
 echo "Temp actuelle : $TEMP_ACTUELLE"
 echo "Temp de demain : $TEMP_DEMAIN"
 
+
+#Formatage des données récupérées
 DATE=$(date '+%d-%m-%Y' )
 HEURE=$(date '+%H:%M' )
 
 OUTPUT="$DATE -  $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_DEMAIN"
 echo "$OUTPUT"
+
+#Enregistrement dans meteo.txt
+echo "$OUTPUT" >> meteo.txt
+
