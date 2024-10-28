f3a75b8f851525fd7adbe4adf5849b5834ba101c
yassine <yassinejedidi2004@gmail.com>
2024-10-28 01:26:38 +0100

    Formatage des informations récupérées

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -8,4 +8,10 @@ TEMP_ACTUELLE=$(grep -oP '(?<=\+)\d+°C' meteo_brute.txt | head -n1)
 TEMP_DEMAIN=$(curl -s "wttr.in/$VILLE?format=%t&forecast=1&day=1" | grep -oP '\+\d+°C')
 
 echo "Temp actuelle : $TEMP_ACTUELLE"
-echo "Temp de demain : $TEMP_DEMAIN" 
+echo "Temp de demain : $TEMP_DEMAIN"
+
+DATE=$(date '+%d-%m-%Y' )
+HEURE=$(date '+%H:%M' )
+
+OUTPUT="$DATE -  $HEURE - $VILLE : $TEMP_ACTUELLE - $TEMP_DEMAIN"
+echo "$OUTPUT"
